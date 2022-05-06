2bcd04e491a559c7b5da96d72a1c9a27a8e33017
James Spencer <jameskcs1254@gmail.com>
2022-05-06 15:08:44 +0100

    Edit formatting

--- recursiveReverseString.js
+++ recursiveReverseString.js
@@ -1,10 +1,10 @@
 const reverseString = (string) => {
-  if (string === '' || string.length === 1) { 
+  if (string === "" || string.length === 1) {
     return string;
   } else {
-    return reverseString(string.substring(1)) + string[0]
+    return reverseString(string.substring(1)) + string[0];
   }
-}
+};
 
 /* 
   Although the question says not to use built in string functions I could not find a way in JavaScript to return 
