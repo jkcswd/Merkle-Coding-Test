d0a672bce59032b8cad2a4c70f4767e95c0edacc
James Spencer <jameskcs1254@gmail.com>
2022-05-06 14:01:49 +0100

    Complete reverseString function

---
+++ recursiveReverseString.js
@@ -0,0 +1,12 @@
+const reverseString = (string) => {
+  if (string === '' || string.length === 1) { 
+    return string;
+  } else {
+    return reverseString(string.substring(1)) + string[0]
+  }
+}
+
+/* 
+  Although the question says not to use built in string functions I could not find a way in JavaScript to return 
+  a string without its first character without re-implementing the substring function from scratch.
+*/
